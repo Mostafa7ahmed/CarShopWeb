646d8ae800108a709b0fc102b0b908bcf3ead234
mostafa hamed <Mostafa7ahmed>
2025-01-17 17:18:28 +0200

    Done Transalet

--- src/app/Pages/cardetails/cardetails.component.ts
+++ src/app/Pages/cardetails/cardetails.component.ts
@@ -3,16 +3,33 @@ import { ActivatedRoute } from '@angular/router';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { AllcarService } from '../../Core/services/allcar.service';
 import { Car } from '../../Core/Interface/car';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cardetails',
   standalone: true,
-  imports: [CarouselModule],
+  imports: [CarouselModule , TranslateModule],
   templateUrl: './cardetails.component.html',
   styleUrls: ['./cardetails.component.scss'] // Fixed 'styleUrl' to 'styleUrls'
 })
 export class CardetailsComponent implements OnInit {
 
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    rtl:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause: true,
+    dots: true,
+    navSpeed: 2000,
+    navText: ['', ''],
+  
+    nav: false,
+    items:1,
+   }
   private readonly _activatedRoute = inject(ActivatedRoute);
   private readonly _allcarService = inject(AllcarService);
   car: Car = {} as Car;
