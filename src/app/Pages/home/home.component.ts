28cecf8023195680906752373c62a35155888e18
mostafa hamed <Mostafa7ahmed>
2025-01-18 03:01:31 +0200

    Done WebSite

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { BannerComponent } from "../../Components/banner/banner.component";
 import { SliderComponent } from "../../Components/slider/slider.component";
 import { BodyComponent } from "../../Components/notfound/body/body.component";
 import { CartypeComponent } from "../../Components/cartype/cartype.component";
+import { ContactComponent } from "../../Components/contact/contact.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent, SliderComponent, BodyComponent, CartypeComponent],
+  imports: [BannerComponent, SliderComponent, BodyComponent, CartypeComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
